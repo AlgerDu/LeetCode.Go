e8247c1b763c087118c07d97e467c3af79522862
Alger <AlgerDu@outlook.com>
2021-11-18 22:32:18 +0800

    初步完成

--- 391_isRectangleCover/391_test.go
+++ 391_isRectangleCover/391_test.go
@@ -1,14 +1,42 @@
 package leetcode
 
 import (
+	"strconv"
+	"strings"
 	"testing"
 )
 
+func Anaylse(arrayStr string) [][]int {
+
+	arrayStr = strings.ReplaceAll(arrayStr, "[", "")
+	arrayStr = strings.ReplaceAll(arrayStr, "]", "")
+
+	numArray := strings.Split(arrayStr, ",")
+
+	count := len(numArray)
+	array := make([][]int, count/4)
+
+	for i := 0; i < count; i++ {
+
+		if i%4 == 0 {
+			array[i/4] := make([]int, 4)
+		}
+
+		num, _ := strconv.Atoi(numArray[i])
+
+		array[i/4][i%4] = num
+	}
+
+	return array
+}
+
 func TestExample_1(t *testing.T) {
+
+	inputStr := "[[1,1,3,3],[3,1,4,2],[3,2,4,4],[1,3,2,4],[2,3,3,4]]"
 
 	desire := true
 
-	result := isRectangleCover(make([][]int, 1))
+	result := isRectangleCover(Anaylse(inputStr))
 
 	if desire != result {
 		t.Errorf("result %t is not equal %t", result, desire)
--- 391_isRectangleCover/isRectangleCover.go
+++ 391_isRectangleCover/isRectangleCover.go
@@ -3,51 +3,150 @@ package leetcode
 func isRectangleCover(rectangles [][]int) bool {
 
 	rectangleCount := len(rectangles)
-	lineCount := rectangleCount * 4
-	edges := make([]bool, 4)
-	lineChecked := make([]bool, lineCount)
+	edgeCount := rectangleCount * 4
 
-	paraleLines := make([]int, rectangleCount)
-	paraleLines2 := make([]int, rectangleCount)
+	resultEdges := make([]bool, 4)
+	edgeCheckState := make([]bool, edgeCount)
 
-	for i := 0; i < lineCount; i++ {
+	group1 := make([]int, rectangleCount)
+	group2 := make([]int, rectangleCount)
 
-		if lineChecked[i] {
+	for i := 0; i < edgeCount; i++ {
+
+		if edgeCheckState[i] {
 			continue
 		}
 
-		paraleLines[0] = i
+		group1[0] = i
 
-		edgeIndex := i % 4
+		edgeCode := i % 4
 		rectangleIndex := i / 4
-		count := 1
-		count2 := 0
 
-		m := (4 - edgeIndex) % 4
-		n := rectangles[rectangleIndex][m]
+		group1Count := 1
+		group2Count := 0
+
+		vIndex := (4 - edgeCode) % 4
+		v := rectangles[rectangleIndex][vIndex]
 
 		for j := rectangleIndex + 1; j < rectangleCount; j++ {
 
-			checkLineIndex := rectangleIndex*4 + edgeIndex
+			toCheckEdgeIndex := rectangleIndex*4 + edgeCode
 
-			if !lineChecked[checkLineIndex] && rectangles[j][m] == n {
-				lineChecked[checkLineIndex] = true
-				count++
-				paraleLines[count] = checkLineIndex
+			if !edgeCheckState[toCheckEdgeIndex] && rectangles[j][vIndex] == v {
+				edgeCheckState[toCheckEdgeIndex] = true
+				group1[group1Count] = toCheckEdgeIndex
+				group1Count++
 				continue
 			}
 
-			checkLineIndex = rectangleIndex*4 + (edgeIndex+2)%4
+			toCheckEdgeIndex = rectangleIndex*4 + (edgeCode+2)%4
 
-			if !lineChecked[checkLineIndex] && rectangles[j][(m+2)%4] == n {
-				lineChecked[checkLineIndex] = true
-				count2++
-				paraleLines2[count2] = checkLineIndex
+			if !edgeCheckState[toCheckEdgeIndex] && rectangles[j][(vIndex+2)%4] == v {
+				edgeCheckState[toCheckEdgeIndex] = true
+				group2[group1Count] = toCheckEdgeIndex
+				group2Count++
 			}
 		}
 
-		if count2 == 0 && edges[edgeIndex] {
-			// 找到了重复的边
+		if group2Count == 0 && resultEdges[edgeCode] {
+			return false
+		}
+
+		vStart := (edgeCode + 1) / 4
+		vEnd := (edgeCode + 3) / 4
+
+		edgeStartValue := rectangles[group1[0]/4][vStart]
+
+		for j := 1; j < group1Count; j++ {
+
+			toCompare := rectangles[group1[0]/4][vStart]
+
+			if toCompare < edgeStartValue {
+				edgeStartValue = toCompare
+				tmpIndex := group1[0]
+				group1[0] = group1[j]
+				group1[j] = tmpIndex
+			}
+		}
+
+		edgeEndValue := rectangles[group1[0]/4][vEnd]
+
+		for j := 1; j < group1Count; j++ {
+			k := j
+			for ; k < group1Count; k++ {
+				toCompare := rectangles[group1[k]/4][vStart]
+
+				if toCompare < edgeEndValue {
+					return false
+				}
+
+				if toCompare == edgeEndValue {
+					break
+				}
+			}
+
+			if k == group1Count {
+				return false
+			}
+
+			edgeEndValue = rectangles[group1[k]/4][vEnd]
+
+			tmpIndex := group1[j]
+			group1[j] = group1[k]
+			group1[k] = tmpIndex
+		}
+
+		if group2Count == 0 {
+			resultEdges[edgeCode] = true
+			continue
+		}
+
+		edge2StartValue := rectangles[group2[0]/4][vStart]
+
+		for j := 1; j < group2Count; j++ {
+
+			toCompare := rectangles[group2[0]/4][vStart]
+
+			if toCompare < edge2StartValue {
+				edge2StartValue = toCompare
+				tmpIndex := group2[0]
+				group2[0] = group2[j]
+				group2[j] = tmpIndex
+			}
+		}
+
+		if edgeStartValue != edge2StartValue {
+			return false
+		}
+
+		edge2EndValue := rectangles[group2[0]/4][vEnd]
+
+		for j := 1; j < group2Count; j++ {
+			k := j
+			for ; k < group2Count; k++ {
+				toCompare := rectangles[group2[k]/4][vStart]
+
+				if toCompare < edge2EndValue {
+					return false
+				}
+
+				if toCompare == edge2EndValue {
+					break
+				}
+			}
+
+			if k == group2Count {
+				return false
+			}
+
+			edge2EndValue = rectangles[group2[k]/4][vEnd]
+
+			tmpIndex := group2[j]
+			group2[j] = group2[k]
+			group2[k] = tmpIndex
+		}
+
+		if edgeEndValue != edge2EndValue {
 			return false
 		}
 	}
